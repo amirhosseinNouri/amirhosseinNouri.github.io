ee0d9d210012191b8b8dbeb5378bf2534410ba2d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2026-08-15 16:38:16 +0330

    feat(footer): build SiteFooter with typed socials data (#24) (#58)

---
+++ app/data/socials.ts
@@ -0,0 +1,33 @@
+export interface Social {
+  label: string
+  href: string
+  icon: string
+}
+
+export const socials: Social[] = [
+  {
+    label: 'LinkedIn',
+    href: 'https://linkedin.com/in/amirhosseinnouri2000',
+    icon: 'i-simple-icons-linkedin'
+  },
+  {
+    label: 'GitHub',
+    href: 'https://github.com/amirhosseinnouri',
+    icon: 'i-simple-icons-github'
+  },
+  {
+    label: 'X',
+    href: 'https://x.com/amirhosein_nr',
+    icon: 'i-simple-icons-x'
+  },
+  {
+    label: 'Telegram',
+    href: 'https://t.me/Amirhossein_nr',
+    icon: 'i-simple-icons-telegram'
+  },
+  {
+    label: 'Email',
+    href: 'mailto:amir.h.nouri2000@gmail.com',
+    icon: 'i-lucide-mail'
+  }
+]
