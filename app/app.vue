85fc6980e239bb99c6d54d5133919099594be38b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2026-08-15 17:15:22 +0330

    fix(assets): reference manifest and icon set in head; correct manifest name (#34)

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+  ],
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
